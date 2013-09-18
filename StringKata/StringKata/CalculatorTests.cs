67b3734bac5f1014f74c92c806cd851b878fdd65
Riaan Hanekom <rhanekom@gmail.com>
2013-09-18 17:09:52 +0200

    Empty string returns 0.

--- StringKata/StringKata/CalculatorTests.cs
+++ StringKata/StringKata/CalculatorTests.cs
@@ -5,6 +5,30 @@ namespace StringKata
     [TestFixture]
     public class CalculatorTests
     {
+        #region Globals
 
+        private Calculator subject;
+
+        #endregion
+
+        #region Setup
+
+        [SetUp]
+        public void Setup()
+        {
+            subject = new Calculator();
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Add_Returns_0_For_Empty_String()
+        {
+            Assert.That(subject.Add(string.Empty), Is.EqualTo(0));
+        }
+
+        #endregion
     }
 }
