4bf81dc76c34a691ba730c8deeec4689f87463ae
Riaan Hanekom <rhanekom@gmail.com>
2013-09-19 14:48:08 +0200

    Console checks parameters.

--- StringKata/StringKata.Console/Program.cs
+++ StringKata/StringKata.Console/Program.cs
@@ -1,9 +1,30 @@
 ﻿namespace StringKata.Console
 {
+    using Core;
+
     public class Program
     {
+        #region Construction
+        
+        static Program()
+        {
+            Output = new ConsoleOutput();
+        }
+
+        #endregion
+
+        #region Public Members
+
+        public static IOutput Output { get; set; }
+ 
         public static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Output.Write("Usage : scalc '1,2,3'");
+            }
         }
+
+        #endregion
     }
 }
---
+++ StringKata/StringKata.Tests/Console/ProgramTests.cs
@@ -0,0 +1,41 @@
+﻿namespace StringKata.Tests.Console
+{
+    using Moq;
+    using NUnit.Framework;
+    using StringKata.Console;
+    using StringKata.Core;
+
+    [TestFixture]
+    public class ProgramTests
+    {
+        #region Globals
+
+        private Mock<IOutput> output;
+
+        #endregion
+        
+        #region Setup
+
+        [SetUp]
+        public void Setup()
+        {
+            output = new Mock<IOutput>();
+            Program.Output = output.Object;
+        }
+
+        #endregion
+
+        #region Tests
+        
+        [Test]
+        public void Main_Writes_Usage_If_No_Parameters_Specified()
+        {
+            output.Setup(x => x.Write(It.Is((string s) => s.Contains("Usage"))));
+            
+            Program.Main(new string[0]);
+            output.VerifyAll();
+        }
+
+        #endregion
+    }
+}
