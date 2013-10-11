9f9b297646b17c51b5646cfbaf715a792f6e7a7c
Riaan Hanekom <rhanekom@gmail.com>
2013-10-11 15:56:15 +0200

    Golden Master test added.

---
+++ GildedRose/GildedRose.Tests/GildedRoseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GildedRose.Tests
+{
+    using System.IO;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GildedRoseTests
+    {
+        [Test]
+        public void GoldenMasterTest()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            
+            Program.Main(null);
+
+            Assert.That(writer.ToString(), Is.EqualTo(File.ReadAllText("output.txt")));
+        }
+    }
+}
--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -1,111 +1,110 @@
-using System;
-using System.Collections;
-
 namespace GildedRose
 {
-	public class GildedRose
-	{	    
+    using System;
 
-	    public void UpdateQuality()
-	    {
-	        var uri = "http://www.gildrose.co.za/api/inventory";
-		    var items = GildedRoseWebService.GetInventory(uri);
-			for (var i = 0; i < items.Count; i++)
-			{
-				if (items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-				{
-					if (items[i].Quality > 0)
-					{
-						if (items[i].Name != "Sulfuras, Hand of Ragnaros")
-						{
-							items[i].Quality = items[i].Quality - 1;
-						}
-					}
-				}
-				else
-				{
-					if (items[i].Quality < 50)
-					{
-						items[i].Quality = items[i].Quality + 1;
-						
-						if (items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-						{
-							if (items[i].SellIn < 11)
-							{
-								if (items[i].Quality < 50)
-								{
-									items[i].Quality = items[i].Quality + 1;
-								}
-							}
-							
-							if (items[i].SellIn < 5)
-							{
-								if (items[i].Quality < 50)
-								{
-									items[i].Quality = items[i].Quality + 1;
-								}
-							}
-						}
-					}
-				}
-				
-				if (items[i].Name != "Sulfuras, Hand of Ragnaros")
-				{
-					items[i].SellIn = items[i].SellIn - 1;
-				}
-				
-				if (items[i].SellIn < 0)
-				{
-					if (items[i].Name != "Aged Brie")
-					{
-						if (items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-						{
-							if (items[i].Quality > 0)
-							{
-								if (items[i].Name != "Sulfuras, Hand of Ragnaros")
-								{
-									items[i].Quality = items[i].Quality - 1;
-								}
-							}
-						}
-						else
-						{
-							items[i].Quality = items[i].Quality - items[i].Quality;
-						}
-					}
-					else
-					{
-						if (items[i].Quality < 50)
-						{
-							items[i].Quality = items[i].Quality + 1;
-						}
-					}
-				}
-			}
+    public class GildedRose
+    {
+        public void UpdateQuality()
+        {
+            const string uri = "http://www.gildrose.co.za/api/inventory";
+            var items = GildedRoseWebService.GetInventory(uri);
+            
+            foreach (Item item in items)
+            {
+                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
 
-	        GildedRoseWebService.SaveInventory(uri, items);
-	    }
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.SellIn < 11)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality = item.Quality + 1;
+                                }
+                            }
 
-	    public void PrintReport()
-	    {
-	        var items = GildedRoseWebService.GetInventory("http://www.gildedrose.co.za/api/inventory/");
-	        Console.WriteLine("name, sellIn, quality");
-	        for (var j = 0; j < items.Count; j++)
-	        {
-	            Console.WriteLine(items[j].Name + ", " + items[j].SellIn + ", " + items[j].Quality);
-	        }
-	        Console.WriteLine("");
-	    }
-	}
+                            if (item.SellIn < 5)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality = item.Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn = item.SellIn - 1;
+                }
+
+                if (item.SellIn < 0)
+                {
+                    if (item.Name != "Aged Brie")
+                    {
+                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.Quality > 0)
+                            {
+                                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    item.Quality = item.Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            item.Quality = item.Quality - item.Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+                }
+            }
+
+            GildedRoseWebService.SaveInventory(uri, items);
+        }
+
+        public void PrintReport()
+        {
+            var items = GildedRoseWebService.GetInventory("http://www.gildedrose.co.za/api/inventory/");
+            Console.WriteLine("name, sellIn, quality");
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
+            }
+
+            Console.WriteLine("");
+        }
+    }
 
 
     public class Item
-	{
-		public string Name { get; set; }
-		
-		public int SellIn { get; set; }
-		
-		public int Quality { get; set; }
-	}
-	
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
 }
